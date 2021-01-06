beeccf6f8acb2c97d997b852e117e6036661726a
NutchaPK <theuoon9899@gmail.com>
2021-01-07 00:57:47 +0700

    Frontend

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,19 +1,22 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
-import WatchVideo from './components/WatchVideo'
+
 import SignIn from './components/SignIn'
+import CreateBill from './components/Bill'
 
 //createHistorytaking
 import  create_Patientrights from './components/create_patientrights';
-import  createHistorytaking from './components/createHistorytaking';
+
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
     router.registerRoute('/', WelcomePage);
-    router.registerRoute('/watch_video', WatchVideo);
+   
     router.registerRoute('/signin', SignIn);
+    router.registerRoute('/createbill', CreateBill);
+
     router.registerRoute('/create_Patientrights', create_Patientrights);
-    router.registerRoute('/createHistorytaking', createHistorytaking);
+    
   },
 });
